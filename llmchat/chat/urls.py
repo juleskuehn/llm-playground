773e3b651d095a4c725edc1b677f796c591bcdd5
Jules Kuehn <jk@jules.lol>
2023-08-12 21:53:01 -0400

    finish chat UI (no bot yet)

--- llmchat/chat/urls.py
+++ llmchat/chat/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views, magiclink_views
 
 urlpatterns = [
-    path("", views.IndexView.as_view(), name="index"),
+    path("", views.NewChatView.as_view(), name="index"),
     path("login/", magiclink_views.PhacOnlyLoginView.as_view(), name="phac_only_login"),
     path("logout/", views.logout_view, name="logout"),
-    path("chat/", views.ChatView.as_view(), name="chat"),
+    path("chat/<int:chat_id>", views.ChatView.as_view(), name="chat"),
 ]
--- llmchat/chat/views.py
+++ llmchat/chat/views.py
@@ -4,25 +4,66 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth import logout
 
 from chat.forms import MessageForm
+from chat.models import Message, User, Chat
+
 
 class IndexView(LoginRequiredMixin, TemplateView):
-    # template_name = "index.jinja"
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["form"] = MessageForm()
-        return context
-    
-    template_name = "chat.jinja"
+    template_name = "index.jinja"
 
 
 class ChatView(LoginRequiredMixin, TemplateView):
     # Add a form to the context
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context["chat"] = Chat.objects.get(id=kwargs["chat_id"])
         context["form"] = MessageForm()
         return context
     
+    def post(self, request, *args, **kwargs):
+        form = MessageForm(request.POST)
+        if form.is_valid():
+            message = Message.objects.create(
+                message=form.cleaned_data["message"],
+                chat_id=kwargs["chat_id"],
+            )
+            messages = Message.objects.filter(chat_id=kwargs["chat_id"])
+            return render(request, "fragments/messages.jinja", {"messages": messages})
+        else:
+            return self.render_to_response({"form": form})
+
     template_name = "chat.jinja"
+
+
+class NewChatView(ChatView):
+    """
+    Creates a new chat on GET and renders the chat template
+    """
+    def get(self, request, *args, **kwargs):
+        # Create a new chat
+        chat = Chat.objects.create(user=request.user)
+        # Redirect to the chat view
+        return redirect("chat", chat_id=chat.id)
+
+
+class SingleMessageView(LoginRequiredMixin, TemplateView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["message"] = Message.objects.get(id=kwargs["message_id"])
+        return context
+
+    def post(self, request, *args, **kwargs):
+        form = MessageForm(request.POST)
+        if form.is_valid():
+            message = Message.objects.create(
+                author=request.user,
+                message=form.cleaned_data["message"],
+                chat_id=kwargs["chat_id"],
+            )
+            return render(request, "single_message.jinja", {"message": message})
+        else:
+            return self.render_to_response({"form": form})
+
+    template_name = "single_message.jinja"
 
 
 def logout_view(request):
